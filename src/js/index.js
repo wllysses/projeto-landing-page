7d75edaf19fe39e244e09a2f00edd4acddf29d9b
wllysses <wllyssestavares@gmail.com>
2022-05-14 16:40:14 -0300

    alteracoes-inclusao-js

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+const evento1 = document.getElementById('evento1')
+
+evento1.addEventListener('mouseenter', () => {
+    document.getElementById('teste1').style.display = 'block'
+})
+
+evento1.addEventListener('mouseout', () => {
+    document.getElementById('teste1').style.display = 'none'
+})
+
+const evento2 = document.getElementById('evento2')
+
+evento2.addEventListener('mouseenter', () => {
+    document.getElementById('teste2').style.display = 'block'
+})
+
+evento2.addEventListener('mouseout', () => {
+    document.getElementById('teste2').style.display = 'none'
+})
+
+const evento3 = document.getElementById('evento3')
+
+evento3.addEventListener('mouseenter', () => {
+    document.getElementById('teste3').style.display = 'block'
+})
+
+evento3.addEventListener('mouseout', () => {
+    document.getElementById('teste3').style.display = 'none'
+})
+
+const evento4 = document.getElementById('evento4')
+
+evento4.addEventListener('mouseenter', () => {
+    document.getElementById('teste4').style.display = 'block'
+})
+
+evento4.addEventListener('mouseout', () => {
+    document.getElementById('teste4').style.display = 'none'
+})
+
