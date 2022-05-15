bc39ecf62b9626c055fbe8a0ee07fe8dc91eb92b
wllysses <wllyssestavares@gmail.com>
2022-05-15 09:33:42 -0300

    alteracoes-css-js-15/05

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,39 @@
-const evento1 = document.getElementById('evento1')
+const evento1 = document.getElementById('classic-event')
 
 evento1.addEventListener('mouseenter', () => {
-    document.getElementById('teste1').style.display = 'block'
+    document.getElementById('classic-ingredientes').style.display = 'block'
 })
 
 evento1.addEventListener('mouseout', () => {
-    document.getElementById('teste1').style.display = 'none'
+    document.getElementById('classic-ingredientes').style.display = 'none'
 })
 
-const evento2 = document.getElementById('evento2')
+const evento2 = document.getElementById('especial-event')
 
 evento2.addEventListener('mouseenter', () => {
-    document.getElementById('teste2').style.display = 'block'
+    document.getElementById('especial-ingredientes').style.display = 'block'
 })
 
 evento2.addEventListener('mouseout', () => {
-    document.getElementById('teste2').style.display = 'none'
+    document.getElementById('especial-ingredientes').style.display = 'none'
 })
 
-const evento3 = document.getElementById('evento3')
+const evento3 = document.getElementById('prime-event')
 
 evento3.addEventListener('mouseenter', () => {
-    document.getElementById('teste3').style.display = 'block'
+    document.getElementById('prime-ingredientes').style.display = 'block'
 })
 
 evento3.addEventListener('mouseout', () => {
-    document.getElementById('teste3').style.display = 'none'
+    document.getElementById('prime-ingredientes').style.display = 'none'
 })
 
-const evento4 = document.getElementById('evento4')
+const evento4 = document.getElementById('double-event')
 
 evento4.addEventListener('mouseenter', () => {
-    document.getElementById('teste4').style.display = 'block'
+    document.getElementById('double-ingredientes').style.display = 'block'
 })
 
 evento4.addEventListener('mouseout', () => {
-    document.getElementById('teste4').style.display = 'none'
+    document.getElementById('double-ingredientes').style.display = 'none'
 })
-
