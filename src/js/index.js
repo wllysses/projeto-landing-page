03a126a9fc854d23240df1578920822660a2bd52
wllysses <wllyssestavares@gmail.com>
2022-05-17 10:29:50 -0300

    refatoracao-html-css-js(17/05/22)

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,37 @@
-const evento1 = document.getElementById('classic-event')
-
-evento1.addEventListener('mouseenter', () => {
-    document.getElementById('classic-ingredientes').style.display = 'block'
- })
+function eventoBurger(burger) {
     
-evento1.addEventListener('mouseout', () => {
-    document.getElementById('classic-ingredientes').style.display = 'none'
-})
+    if(burger === classic) {
+        burger.addEventListener('mouseenter', () => {
+            document.querySelector('#ingredientes-classic').style.opacity = '1'
+        })
+        burger.addEventListener('mouseout', () => {
+            document.querySelector('#ingredientes-classic').style.opacity = '0'
+        })
+    } else if(burger === especial) {
+        burger.addEventListener('mouseenter', () => {
+            document.querySelector('#ingredientes-especial').style.opacity = '1'
+        })
+        burger.addEventListener('mouseout', () => {
+            document.querySelector('#ingredientes-especial').style.opacity = '0'
+        })
+    } else if(burger === prime) {
+        burger.addEventListener('mouseenter', () => {
+            document.querySelector('#ingredientes-prime').style.opacity = '1'
+        })
+        burger.addEventListener('mouseout', () => {
+            document.querySelector('#ingredientes-prime').style.opacity = '0'
+        })
+    } else {
+        burger.addEventListener('mouseenter', () => {
+            document.querySelector('#ingredientes-double').style.opacity = '1'
+        })
+        burger.addEventListener('mouseout', () => {
+            document.querySelector('#ingredientes-double').style.opacity = '0'
+        })
+    }
+}
 
-const evento2 = document.getElementById('especial-event')
-
-evento2.addEventListener('mouseenter', () => {
-    document.getElementById('especial-ingredientes').style.display = 'block'
-})
-
-evento2.addEventListener('mouseout', () => {
-    document.getElementById('especial-ingredientes').style.display = 'none'
-})
-
-const evento3 = document.getElementById('prime-event')
-
-evento3.addEventListener('mouseenter', () => {
-    document.getElementById('prime-ingredientes').style.display = 'block'
-})
-
-evento3.addEventListener('mouseout', () => {
-    document.getElementById('prime-ingredientes').style.display = 'none'
-})
-
-const evento4 = document.getElementById('double-event')
-
-evento4.addEventListener('mouseenter', () => {
-    document.getElementById('double-ingredientes').style.display = 'block'
-})
-
-evento4.addEventListener('mouseout', () => {
-    document.getElementById('double-ingredientes').style.display = 'none'
-})
+eventoBurger(classic)
+eventoBurger(especial)
+eventoBurger(prime)
+eventoBurger(double)
